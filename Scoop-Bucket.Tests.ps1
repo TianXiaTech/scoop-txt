0d5f159ef2e9155570895f59f3a1f4a78f16464b
aliuq <aliuq@bilishare.com>
2026-03-20 17:01:07 +0800

    scoop-bucket-tests: add BucketPath parameter to Import-Bucket-Tests

--- Scoop-Bucket.Tests.ps1
+++ Scoop-Bucket.Tests.ps1
@@ -1,2 +1,2 @@
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
+. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1" -BucketPath "$PSScriptRoot/bucket"
