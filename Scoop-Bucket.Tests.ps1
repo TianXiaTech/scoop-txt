c37dee2783aea9504679155dbb839761df2f6ea2
aliuq <aliuq@bilishare.com>
2026-09-08 17:29:48 +0800

    (chore): Fix CI manifest validation

--- Scoop-Bucket.Tests.ps1
+++ Scoop-Bucket.Tests.ps1
@@ -1,2 +1,16 @@
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+function Get-GitChangedFile {
+  param(
+    [string] $Path,
+    [string] $Commit,
+    [string[]] $Include
+  )
+
+  $bucketPath = [IO.Path]::GetFullPath($Path).TrimEnd('\')
+  & BuildHelpers\Get-GitChangedFile -Path $Path -Commit $Commit -Include $Include |
+    Where-Object {
+      $filePath = [IO.Path]::GetFullPath($_)
+      $filePath.StartsWith("$bucketPath\", [StringComparison]::OrdinalIgnoreCase)
+    }
+}
 . "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1" -BucketPath "$PSScriptRoot/bucket"
